9d9b47f17bd271c2f7f7a988226287a88e5885c9
Ana Lastoviria <ana.lastoviria@zendesk.com>
2022-07-25 09:45:28 +1000

    implement on and trigger

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'Ana' })
+user.on('change', () => console.log("Change 1"));
+user.on('change', () => console.log("Change 2"));
+user.on('save', () => console.log("Saved"));
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('save');
